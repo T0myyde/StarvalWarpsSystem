a5978ba423b55470458f6a92239e7aadb0ca8429
t0myyde <t0myyde@gmail.com>
2022-10-23 18:10:00 +0200

    initial Commit

--- src/main/java/ch/tom/tcodes/warpstarvalcity/utils/Messages.java
+++ src/main/java/ch/tom/tcodes/warpstarvalcity/utils/Messages.java
@@ -11,21 +11,21 @@ public class Messages {
     }
 
     public String getConfigMessage(String path) {
-        String message = null;
+        String message;
         String configMessage = getPrefix() + plugin.getConfig().getString(path);
         message = configMessage.replace("&", "§");
         return message;
     }
 
     public String getConfigMessageNoPrefix(String path) {
-        String message = null;
+        String message;
         String configMessage = plugin.getConfig().getString(path);
         message = configMessage.replace("&", "§");
         return message;
     }
 
     public String getPrefix() {
-        String message = null;
+        String message;
         String configMessage = plugin.getConfig().getString("messages.prefix");
         message = configMessage.replace("&", "§");
         return message;
